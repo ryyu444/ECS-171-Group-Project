6a9195fc8e6dba06286eb74bb2e973970ff23a8b
alexisbl14 <ablydon@ucdavis.edu>
2023-11-26 22:09:51 -0800

    add icon and title

--- frontend/sleep-and-stress/src/app/layout.tsx
+++ frontend/sleep-and-stress/src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="sleepymoon.png" />
+        <title>Stress Level Predictor</title>
+      </head>      
       <body className={inter.className}>{children}</body>
     </html>
   )
